1a42e7cc2dee8c8ed6bf419cf8ab38277aff9f72
Shahidul <theshahidkhan.2004@gmail.com>
2025-07-25 00:18:02 +0600

    added the chosen check

--- Practice/src/Challenge.java
+++ Practice/src/Challenge.java
@@ -27,17 +27,30 @@ public class Challenge {
 
         System.out.println("How surprising, you have passed the first challenge.");
         double radius = Math.round(rd.nextDouble(1.1,10) * 100) / 100.0;
-        double area = Math.PI * Math.pow(radius, 2);
-        System.out.printf("The area of the circle with a radius of %fm is: ", radius);
+        double area = Math.round(Math.PI * Math.pow(radius, 2) * 100) / 100.0;
+        System.out.printf("The area of the circle with a radius of %.2fm is: ", radius);
         double inArea = sc.nextDouble();
         sc.nextLine();
         if (inArea != area) {
             System.out.println("Like many before you. You have failed!");
-            System.exit(3);
+            System.exit(4);
         }
 
         System.out.println("You've impressed me challenger!\nMay I know your name?");
         String Name = sc.nextLine();
+        String vowels = "aeiou";
+        boolean chosen = false;
+        for (int i = 0; i < vowels.length(); i++) {
+            if(Name.contains(vowels.charAt(i)+"")) //same as Name.contains(Character.toString(vowels.charAt(i))
+            {
+                chosen = true; break;
+            }
+        }
+        if(!chosen) {
+            System.out.println("You are not one of the chosen");
+            System.exit(6);
+        }
+        System.out.println("You are one of the chosen ones.\nBut your trials are far from over!");
 
         sc.close();
     }
