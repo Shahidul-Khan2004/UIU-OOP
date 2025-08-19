0431f9b7728130d0f70677d065a17bec74e5b9eb
Shahidul <theshahidkhan.2004@gmail.com>
2025-08-19 23:46:12 +0600

    checked Point class

---
+++ Practice/src/CT1_Practice/Lab_Practice_Sheet/Main.java
@@ -0,0 +1,9 @@
+package CT1_Practice.Lab_Practice_Sheet;
+
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3,4);
+        System.out.println(p1.distance(p2));
+    }
+}
--- Practice/src/CT1_Practice/Lab_Practice_Sheet/Point.java
+++ Practice/src/CT1_Practice/Lab_Practice_Sheet/Point.java
@@ -3,6 +3,10 @@ package CT1_Practice.Lab_Practice_Sheet;
 public class Point {
     int x;
     int y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     public double distance(Point p) {
         return Math.sqrt(Math.pow((x - p.x), 2) + Math.pow((y - p.y), 2));
     }
