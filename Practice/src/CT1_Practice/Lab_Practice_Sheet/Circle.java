d27d2fc76d9e8733a232027b4ce5939dfcb6e0cb
Shahidul <theshahidkhan.2004@gmail.com>
2025-08-19 23:52:51 +0600

    checked Circle class

--- Practice/src/CT1_Practice/Lab_Practice_Sheet/Circle.java
+++ Practice/src/CT1_Practice/Lab_Practice_Sheet/Circle.java
@@ -3,10 +3,14 @@ package CT1_Practice.Lab_Practice_Sheet;
 public class Circle {
     Point center;
     double radius;
+    Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
     public boolean pointInsideCircle(Point p) {
-        return center.distance(p) > radius;
+        return center.distance(p) < radius;
     }
     public boolean circleInsideCircle(Circle C) {
-        return center.distance(C.center) + C.radius <= radius;
+        return center.distance(C.center) + C.radius < radius;
     }
 }
--- Practice/src/CT1_Practice/Lab_Practice_Sheet/Main.java
+++ Practice/src/CT1_Practice/Lab_Practice_Sheet/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(3,4);
-        System.out.println(p1.distance(p2));
+        Circle c = new Circle(p1, 6);
+        System.out.println(c.pointInsideCircle(p2));
+        Circle c1 = new Circle(p1, 3);
+        System.out.println(c.circleInsideCircle(c1));
     }
 }
