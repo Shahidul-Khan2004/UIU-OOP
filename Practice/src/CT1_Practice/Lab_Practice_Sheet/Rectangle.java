23f9addad13d17d34a92b353941588f0d0090440
Shahidul <theshahidkhan.2004@gmail.com>
2025-08-19 23:31:36 +0600

    added solves to practice sheet

---
+++ Practice/src/CT1_Practice/Lab_Practice_Sheet/Rectangle.java
@@ -0,0 +1,11 @@
+package CT1_Practice.Lab_Practice_Sheet;
+
+public class Rectangle {
+    Point bottomLeft, topRight;
+    public boolean circleInsideRectangle(Circle R) {
+        return R.center.y + R.radius <= topRight.y && R.center.y + R.radius >= bottomLeft.y && R.center.x + R.radius <= topRight.x && R.center.x + R.radius >= bottomLeft.x;
+    }
+    public boolean RectangleInsideRectangle(Rectangle R) {
+        return R.topRight.x <= topRight.x && R.topRight.y <= topRight.y && R.bottomLeft.x >= bottomLeft.x && R.bottomLeft.y >= bottomLeft.y;
+    }
+}
