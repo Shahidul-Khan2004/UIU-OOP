f54a87977feb989c8bd965bb5c4cce9e4b029566
Shahidul <theshahidkhan.2004@gmail.com>
2025-09-02 22:35:01 +0600

    added Circle class

---
+++ Practice/src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle extends Shape2D {
+    Triangle(int base, int height) {
+        super(base, height);
+    }
+    double area() {
+        return 0.5 * super.area();
+    }
+    double perimeter() {
+        return super.perimeter() - super.width;
+    }
+}
