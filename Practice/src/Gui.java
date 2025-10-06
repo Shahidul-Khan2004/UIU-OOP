451bdd006599e0825c72913a8eb819f6af8c8b3d
Shahidul <theshahidkhan.2004@gmail.com>
2025-10-07 00:08:01 +0600

    gui

--- Practice/src/Gui.java
+++ Practice/src/Gui.java
@@ -3,34 +3,48 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class action implements ActionListener {
+class SayHello implements ActionListener{
     @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("Button clicked!");
+    public void actionPerformed(ActionEvent e){
+        System.out.println("Hello!");
+    }
+}
+
+class Frame extends JFrame {
+    Frame(int width, int height) {
+        this.setTitle("My GUI");
+        this.setSize(width, height);
+        this.setVisible(true);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class Title extends JLabel{
+    Title(String title, int x, int y, int width, int height){
+        super(title);
+        this.setBounds(x, y, width, height);
+        this.setFont(new Font("Arial", Font.PLAIN, 30));
+    }
+}
+
+class Say extends JButton{
+    Say(ActionListener action,int x, int y, int width, int height){
+        this.setBounds(x, y, width, height);
+        this.addActionListener(action);
     }
 }
 
 public class Gui {
-    Gui() {
-        JFrame frame = new JFrame("My GUI");
-        JLabel title = new JLabel("My GUI");
-        JButton button = new JButton("Click Me");
-
-        frame.setSize(400, 400);
-        title.setBounds(100, 100, 200, 20);
-        button.setBounds(150, 150, 100, 40);
-
-        frame.setVisible(true);
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(title);
-        frame.add(button);
-
-        title.setFont(new Font("Arial", Font.BOLD, 20));
-        title.setHorizontalAlignment(JLabel.CENTER);
-        button.addActionListener(new action());
-    }
     public static void main(String[] args) {
-        new Gui();
+        Frame frame = new Frame(400,300);
+        Title title = new Title("My GUI",100,50,200,50);
+        Say say = new Say(new SayHello(),175,100,50,50);
+
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setVerticalAlignment(JLabel.CENTER);
+
+        frame.add(say);
+        frame.add(title);
     }
 }
