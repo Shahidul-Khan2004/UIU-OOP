f7bbaefda7b727c8f988acd8348e807c9dbbb50e
Shahidul <theshahidkhan.2004@gmail.com>
2025-10-06 23:39:52 +0600

    gui

--- Practice/src/Gui.java
+++ Practice/src/Gui.java
@@ -1,17 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class action implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Button clicked!");
+    }
+}
 
 public class Gui {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("My GUI Application");
+    Gui() {
+        JFrame frame = new JFrame("My GUI");
+        JLabel title = new JLabel("My GUI");
+        JButton button = new JButton("Click Me");
+
         frame.setSize(400, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        title.setBounds(100, 100, 200, 20);
+        button.setBounds(150, 150, 100, 40);
+
         frame.setVisible(true);
         frame.setLayout(null);
-
-        JLabel title = new JLabel("My GUI Application");
-        title.setBounds(0,0, 200, 30);
-        title.setFont(new Font("Arial", Font.BOLD, 20));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(title);
+        frame.add(button);
+
+        title.setFont(new Font("Arial", Font.BOLD, 20));
+        title.setHorizontalAlignment(JLabel.CENTER);
+        button.addActionListener(new action());
+    }
+    public static void main(String[] args) {
+        new Gui();
     }
 }
